2763d50437ecd31ac91d2ebd78b897b9e78b25b5
AndrejTrajkovski <andrej.trajkovski@hotmail.com>
2019-06-25 20:19:02 +0200

    Initial Commit

---
+++ EasySections/EasySections.h
@@ -0,0 +1,19 @@
+//
+//  EasySections.h
+//  EasySections
+//
+//  Created by Andrej Trajkovski on 6/25/19.
+//  Copyright © 2019 Andrej Trajkovski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EasySections.
+FOUNDATION_EXPORT double EasySectionsVersionNumber;
+
+//! Project version string for EasySections.
+FOUNDATION_EXPORT const unsigned char EasySectionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EasySections/PublicHeader.h>
+
+

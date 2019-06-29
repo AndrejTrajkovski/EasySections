2995260b0c5424e7ed771c7fd052e501d8891f72
AndrejTrajkovski <andrej.trajkovski@hotmail.com>
2019-06-29 15:43:50 +0200

    Fixing access control

--- EasySections/AbstractSectionDelegate.swift
+++ EasySections/AbstractSectionDelegate.swift
@@ -1,27 +1,27 @@
 import UIKit
 
-@objc protocol AbstractSectionDelegateProtocol: UITableViewDelegate, UITableViewDataSource {
+@objc public protocol AbstractSectionDelegateProtocol: UITableViewDelegate, UITableViewDataSource {
     var sectionCount: Int {get}
 }
 
-class AbstractSectionDelegate: NSObject, AbstractSectionDelegateProtocol {
-
-    var sectionCount: Int
-
-    init(_ sectionCount: Int = 1) {
+open class AbstractSectionDelegate: NSObject, AbstractSectionDelegateProtocol {
+    
+    public internal(set) var sectionCount: Int
+    
+    public init(_ sectionCount: Int = 1) {
         self.sectionCount = sectionCount
         super.init()
     }
-
-    func numberOfSections(in tableView: UITableView) -> Int {
+    
+    public func numberOfSections(in tableView: UITableView) -> Int {
         return sectionCount
     }
-
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    
+    open func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         fatalError("should be implemented in subclasses")
     }
-
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    
+    open func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         fatalError("should be implemented in subclasses")
     }
 }
--- EasySections/MainTableViewDelegate.swift
+++ EasySections/MainTableViewDelegate.swift
@@ -1,76 +1,76 @@
 import UIKit
-class MainTableViewDelegate: AbstractSectionDelegate {
-
+public class MainTableViewDelegate: AbstractSectionDelegate {
+    
     private var delegates: [AbstractSectionDelegateProtocol]
     private var zipped: [DelegateWithRange]
-
-    init(_ delegates: [AbstractSectionDelegateProtocol]) {
+    
+    public init(_ delegates: [AbstractSectionDelegateProtocol]) {
         self.delegates = delegates
         zipped = Array(zip(delegates, delegates.makeCorrespondingRanges()))
         super.init(delegates.totalCount())
     }
-
-    func didUpdateData() {
+    
+    public func didUpdateData() {
         zipped = Array(zip(delegates, delegates.makeCorrespondingRanges()))
         super.sectionCount = delegates.totalCount()
     }
-
+    
     // MARK: UITableViewDelegate
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    override public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         let delegate = zipped.delegate(section)
         return delegate.tableView(tableView, numberOfRowsInSection: section)
     }
-
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    
+    override public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let delegate = zipped.delegate(indexPath)
         return delegate.tableView(tableView, cellForRowAt: indexPath)
     }
-
+    
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         let delegate = zipped.delegate(indexPath)
         return delegate.tableView?(tableView, heightForRowAt: indexPath) ?? 44.0
     }
-
+    
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
         let delegate = zipped.delegate(section)
         return delegate.tableView?(tableView, viewForHeaderInSection: section)
     }
-
+    
     func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
         let delegate = zipped.delegate(section)
         return delegate.tableView?(tableView, viewForFooterInSection: section)
     }
-
+    
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
         let delegate = zipped.delegate(section)
         return delegate.tableView?(tableView, heightForHeaderInSection: section) ?? 0
     }
-
+    
     func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
         let delegate = zipped.delegate(section)
         return delegate.tableView?(tableView, heightForFooterInSection: section) ?? 0
     }
-
+    
     func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
         let delegate = zipped.delegate(indexPath)
         delegate.tableView?(tableView, willDisplay: cell, forRowAt: indexPath)
     }
-
+    
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         let delegate = zipped.delegate(section)
         return delegate.tableView?(tableView, titleForHeaderInSection: section)
     }
-
+    
     func tableView(_ tableView: UITableView, titleForFooterInSection section: Int) -> String? {
         let delegate = zipped.delegate(section)
         return delegate.tableView?(tableView, titleForFooterInSection: section)
     }
-
+    
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let delegate = zipped.delegate(indexPath)
         delegate.tableView?(tableView, didSelectRowAt: indexPath)
     }
-
+    
     func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
         let delegate = zipped.delegate(indexPath)
         delegate.tableView?(tableView, didDeselectRowAt: indexPath)
